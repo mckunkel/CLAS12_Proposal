9b6161be80f02aa527206bd010fdf1b7faf4aab1
dlersch <d.lersch@fz-juelich.de>
2016-05-25 15:42:43 +0200

    latest update
    code, plots and your changes

--- plot_stuff.C
+++ plot_stuff.C
@@ -234,6 +234,30 @@ void plot_stuff(){
     hMMPEmX->Draw("same");
     leg2->Draw("same");
     c->cd();
+    
+    TH1D *hIVEPEm = (TH1D*)in->Get("hIVEpEm");
+    TH1D *hIVEPEm_cut = (TH1D*)in->Get("hIVEpEm_cut");
+    TH1D *hEpEm_contam = (TH1D*)in->Get("hEpEm_contam");
+    
+    hIVEpEm->GetXaxis()->SetTitle("M(e^{+}e^{-}) [GeV]");
+    hIVEpEm->GetXaxis()->SetTitleSize(0.05);
+    hIVEpEm->GetXaxis()->SetTitleOffset(0.8);
+    
+    hIVEpEm->GetYaxis()->SetTitle("Counts");
+    hIVEpEm->GetYaxis()->SetTitleSize(0.05);
+    hIVEpEm->GetYaxis()->SetTitleOffset(1.0);
+    
+    TCanvas *c2 = new TCanvas("c2","",1);
+    hIVEpEm->Draw();
+    c2->cd();
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 }

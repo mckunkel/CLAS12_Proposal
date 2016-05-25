10df2be2a3683109f4ade6c2daecd0d7c15bf061
mckunkel <mkunkel@jlab.org>
2016-05-25 18:14:08 +0200

    added final plots i think

--- plot_stuff.C
+++ plot_stuff.C
@@ -170,14 +170,55 @@ void fitMKVoight(TH1 *h33 , Double_t low, Double_t high, Double_t p0, Double_t p
 
 
 
+void plot_stuff(){
 <<<<<<< HEAD
 <<<<<<< HEAD
-void plot_stuff_mk(){
+   
+    gStyle->SetOptStat(0);
+    gStyle->SetOptTitle(0);
+    
+     gROOT->ForceStyle();
+    TFile *in = TFile::Open("IncLusive_Plots_vmd.root");
+    
+    TH1D *hIVEpEmGam = (TH1D*)in->Get("hIVEpEmGam");
+    TH1D *hIVEpEmGam_cut = (TH1D*)in->Get("hIVEpEmGam_cut");
+    
+    hIVEpEmGam->SetLineColor(kBlack);
+    hIVEpEmGam->SetStats(false);
+    hIVEpEmGam_cut->SetStats(false);
+    hIVEpEmGam->GetXaxis()->SetTitle("M(e^{+}e^{-}#gamma) [GeV]");
+    hIVEpEmGam->GetXaxis()->SetTitleSize(0.05);
+    hIVEpEmGam->GetXaxis()->SetTitleOffset(0.8);
+    hIVEpEmGam->GetYaxis()->SetTitle("Counts / 35.5 MeV");
+    hIVEpEmGam->GetYaxis()->SetTitleSize(0.05);
+    hIVEpEmGam->GetYaxis()->SetTitleOffset(1.0);
+    
+    hIVEpEmGam->SetLineWidth(2);
+    
+    hIVEpEmGam_cut->SetLineColor(kRed);
+    hIVEpEmGam_cut->SetFillColor(kRed);
+    hIVEpEmGam_cut->SetFillStyle(3001);
+    
+    TH1D *hMMPEmX = (TH1D*)in->Get("hMMPEmX");
+    TH1D *hMMPEmX_cut = (TH1D*)in->Get("hMMPEmX_cut");
+    
+    hMMPEmX->SetLineColor(kBlack);
+    hMMPEmX->SetLineWidth(2);
+    
+    hMMPEmX->GetXaxis()->SetTitle("M_{x}(pe^{-}) [GeV]");
+    hMMPEmX->SetStats(false);
+    hMMPEmX_cut->SetStats(false);
+    hMMPEmX->GetXaxis()->SetTitleSize(0.05);
+    hMMPEmX->GetXaxis()->SetTitleOffset(0.8);
+    hMMPEmX->GetYaxis()->SetTitle("Counts / 35.5 MeV");
+    hMMPEmX->GetYaxis()->SetTitleSize(0.05);
+    hMMPEmX->GetYaxis()->SetTitleOffset(1.0);
+    
+    hMMPEmX_cut->SetLineColor(kRed);
+    hMMPEmX_cut->SetFillColor(kRed);
+    hMMPEmX_cut->SetFillStyle(3001);
 =======
-void plot_stuff(){
->>>>>>> master
 =======
-void plot_stuff(){
 >>>>>>> master
   
   gStyle->SetOptStat(0);
@@ -186,6 +227,10 @@ void plot_stuff(){
   gROOT->ForceStyle();
   TFile *in = TFile::Open("IncLusive_Plots_vmd.root");
   TFile *inflat = TFile::Open("IncLusive_Plots_flat.root");
+<<<<<<< HEAD
+>>>>>>> master
+=======
+>>>>>>> master
 
   TH1D *hIVEpEmGam = (TH1D*)in->Get("hIVEpEmGam");
   TH1D *hIVEpEmGam_cut = (TH1D*)in->Get("hIVEpEmGam_cut");
@@ -245,6 +290,119 @@ void plot_stuff(){
     if(i>critMMbin){
       MMrest->SetBinContent(i,0);
     }
+<<<<<<< HEAD
+<<<<<<< HEAD
+    MMrest->SetFillStyle(3001);
+    MMrest->SetFillColor(8);
+    MMrest->SetLineColor(8);
+    
+    TLegend *leg1 = new TLegend(0.25,0.7,0.9,0.9);
+    leg1->SetFillColor(0);
+    leg1->AddEntry(IVrest,"Select e^{-}' from e^{-}p#rightarrow e^{-}'pX");
+    leg1->AddEntry(hIVEpEmGam_cut,"Select e^{-} from #eta'#rightarrow e^{+}e^{-}#gamma");
+    
+    TLegend *leg2 = new TLegend(0.25,0.7,0.9,0.9);
+    leg2->SetFillColor(0);
+    leg2->AddEntry(MMrest,"Select e^{-}' from e^{-}p#rightarrow e^{-}'pX");
+    leg2->AddEntry(hMMPEmX_cut,"Select e^{-} from #eta'#rightarrow e^{+}e^{-}#gamma");
+    
+    TCanvas *c = new TCanvas("c","",1200,500);
+    c->Divide(2);
+    c->cd(1);
+    
+    IVrest->Draw();
+    fitMKVoight(hIVEpEmGam, 0.6, 2.1, 0, 0, 0, 0, 0.957, 0.01, 0.001, 2.5, 1);
+    
+    
+    hIVEpEmGam_cut->Draw("same");
+    hIVEpEmGam->Draw("same");
+    leg1->Draw("same");
+    c->cd(2);
+    
+    MMrest->Draw();
+    fitMKVoight(hMMPEmX, 0.6, 2.1, 0, 0, 0, 0, 0.957, 0.01, 0.001, 2.5, 1);
+    
+    hMMPEmX_cut->Draw("same");
+    hMMPEmX->Draw("same");
+    leg2->Draw("same");
+    c->cd();
+    
+    TH1D *hIVEpEm = (TH1D*)in->Get("hIVEpEm");
+    TH1D *hIVEpEm_cut = (TH1D*)in->Get("hIVEpEm_cut");
+    TH1D *hEpEm_contam = (TH1D*)in->Get("hEpEm_contam");
+    
+    
+    hIVEpEm->GetXaxis()->SetTitle("M(e^{+}e^{-}) [GeV]");
+    hIVEpEm->GetXaxis()->SetTitleSize(0.05);
+    hIVEpEm->GetXaxis()->SetTitleOffset(0.8);
+    
+    hIVEpEm->GetYaxis()->SetTitle("Counts / 40 MeV");
+    hIVEpEm->GetYaxis()->SetTitleSize(0.05);
+    hIVEpEm->GetYaxis()->SetTitleOffset(0.8);
+    
+    hIVEpEm->SetLineColor(kBlack);
+    hIVEpEm->SetLineWidth(2);
+
+    
+    hIVEpEm_cut->SetLineColor(kRed);
+    hIVEpEm_cut->SetFillColor(kRed);
+    hIVEpEm_cut->SetFillStyle(3001);
+
+    
+    hEpEm_contam->SetLineColor(8);
+    hEpEm_contam->SetFillColor(8);
+    hEpEm_contam->SetFillStyle(3001);
+    
+    TLegend *leg3 = new TLegend(0.6,0.6,0.9,0.9);
+    leg3->SetFillColor(0);
+    leg3->AddEntry(hIVEpEm_cut,"Select e^{-} from #eta'#rightarrowe^{+}e^{-}#gamma");
+    leg3->AddEntry(hEpEm_contam,"Select e^{-}' from e^{-}p#rightarrow e^{-}'pX");
+  
+    TCanvas *c2 = new TCanvas("c2","",1);
+    hIVEpEm->Draw();
+    hIVEpEm_cut->Rebin(4);
+    hIVEpEm_cut->Draw("same");
+    hEpEm_contam->Rebin(4);
+    hEpEm_contam->Draw("same");
+    hIVEpEm->Draw("same");
+    leg3->Draw("same");
+    c2->SetLogy();
+    
+    
+    TH1D *hEmP = (TH1D*)in->Get("hEmP");
+    TH1D *hEmP_cut = (TH1D*)in->Get("hEmP_cut");
+    
+    hEmP->GetXaxis()->SetTitle("Momentum of e^{-} [GeV/c]");
+    hEmP->GetXaxis()->SetTitleSize(0.05);
+    hEmP->GetXaxis()->SetTitleOffset(0.8);
+    
+    hEmP->GetYaxis()->SetTitle("Counts / 110 MeV");
+    hEmP->GetYaxis()->SetTitleSize(0.05);
+    hEmP->GetYaxis()->SetTitleOffset(0.8);
+    
+    hEmP->SetLineColor(kBlack);
+    hEmP->SetLineWidth(2);
+    
+    hEmP_cut->SetLineColor(kRed);
+    hEmP_cut->SetFillColor(kRed);
+    hEmP_cut->SetFillStyle(3001);
+    
+    TLegend *leg4 = new TLegend(0.6,0.6,0.9,0.9);
+    leg4->SetFillStyle(0);
+    leg4->AddEntry(hEmP_cut,"Select e^{-} from #eta'#rightarrow e^{+}e^{-}#gamma");
+    
+    TCanvas *c3 = new TCanvas("c3","",1);
+    hEmP->Draw();
+    hEmP_cut->Draw("same");
+    leg4->Draw("same");
+    c3->cd();
+    
+    
+    
+    
+=======
+=======
+>>>>>>> master
   }
   MMrest->SetFillStyle(3001);
   MMrest->SetFillColor(8);
@@ -324,6 +482,8 @@ void plot_stuff(){
   hEpEm_corrected->GetXaxis()->SetTitleSize(0.05);
   hEpEm_corrected->Draw("EP");
   hIVEpEm_cut_clone->Draw("EP same");
+<<<<<<< HEAD
+=======
 
   TLegend *legmkII = new TLegend(0.45,0.7,0.9,0.9);
   legmkII->SetTextSize(0.05);
@@ -452,8 +612,141 @@ void plot_stuff(){
   
   TString sLambda_flat = Form("#Lambda^{2}_{fit}  = %2.4f #pm %2.4f  ", Lambda_flat,Lambdaerr_flat);
   TString sbn_flat = Form("b_{n} = %2.4f #pm %2.4f  ", bn_flat,bn_err_flat);
+>>>>>>> master
+
+  TLegend *legmkII = new TLegend(0.45,0.7,0.9,0.9);
+  legmkII->SetTextSize(0.05);
+  legmkII->SetFillColor(0);
+  legmkII->AddEntry(hIVEpEm_cut_clone,"Expected counts in 80 days","l");
+
+  legmkII->AddEntry(hEpEm_corrected,"Accepted corrected counts in 80 days","l");
+  legmkII->Draw("same");
+  
+  
+  TCanvas *cmkIII =  new TCanvas("cmkIII","cmkIII",1200,500);
+  cmkIII->cd();
+  TH1D *hEpEm_acceptance = (TH1D*)in->Get("hEpEm_acceptance");
+  hEpEm_acceptance->SetLineColor(kRed);
+  hEpEm_acceptance->SetYTitle("Acceptance / 10 MeV");
+  hEpEm_acceptance->GetYaxis()->SetTitleSize(0.05);
+  hEpEm_acceptance->GetYaxis()->SetTitleOffset(0.8);
+  hEpEm_acceptance->GetXaxis()->SetTitle("M(e^{+}e^{-}) [GeV]");
+  hEpEm_acceptance->GetXaxis()->SetTitleSize(0.05);
+  hEpEm_acceptance->GetXaxis()->SetTitleOffset(0.8);
+  TH1D *hEpEm_acceptance_flat = (TH1D*)inflat->Get("hEpEm_acceptance");
+  hEpEm_acceptance_flat->SetYTitle("Acceptance / 10 MeV");
+  hEpEm_acceptance_flat->GetYaxis()->SetTitleSize(0.05);
+  hEpEm_acceptance_flat->GetYaxis()->SetTitleOffset(0.8);
+  hEpEm_acceptance_flat->GetXaxis()->SetTitle("M(e^{+}e^{-}) [GeV]");
+  hEpEm_acceptance_flat->GetXaxis()->SetTitleSize(0.05);
+  hEpEm_acceptance_flat->GetXaxis()->SetTitleOffset(0.8);
+  hEpEm_acceptance->GetYaxis()->SetRangeUser(0,0.16);
+  hEpEm_acceptance->Draw("EP");
+  hEpEm_acceptance_flat->Draw("EP same");
+  
+  TLegend *legmkIII = new TLegend(0.11,0.7,0.45,0.9);
+  legmkIII->SetTextSize(0.05);
+  legmkIII->SetFillColor(0);
+  legmkIII->AddEntry(hEpEm_acceptance,"QED+VMD M(e^{+}e^{-}) Acceptance","l");
+  
+  legmkIII->AddEntry(hEpEm_acceptance_flat,"Flat M(e^{+}e^{-}) Acceptance","l");
+  legmkIII->Draw("same");
+  
+  
+  //
+  double QED_par[8] = {0.957}; //{Mass}
+  TF1 *QED_norm = new TF1("QED_norm",Eval_Kroll_wada,0.,1.,1);
+  QED_norm->SetParameters(&QED_par[0]);
+  
+  TH1D *hEpEm_corrected_flat = new TH1D("hEpEm_corrected_flat","hEpEm_corrected_flat",100,0.0,1);
+  TH1D *hEpEm_QEDnorm_flat = new TH1D("hEpEm_QEDnorm_flat","hEpEm_QEDnorm_flat",100,0.0,1);
+  for (int i = 1; i<hIVEpEm_cut_clone->GetNbinsX(); i++) {
+    
+    Double_t bin_factor = 1.65; //this needs to be solved at somepoint
+    Double_t intotal_acceptance = hEpEm_acceptance_flat->GetBinContent(i)*bin_factor;
+    Double_t intotal_events_upper;
+    if (intotal_acceptance == 0) {
+      intotal_events_upper = 0;
+    }else{
+      intotal_events_upper = hIVEpEm_cut_clone->GetBinContent(i)/intotal_acceptance;
+    }
+    hEpEm_corrected_flat->SetBinContent(i,intotal_events_upper);
+    hEpEm_corrected_flat->SetBinError(i,sqrt(intotal_events_upper));
+    
+    Double_t QED_factor = QED_norm->Eval(hIVEpEm_cut_clone->GetBinCenter(i));//2.0e-06;
+    
+    hEpEm_QEDnorm_flat->SetBinContent(i,intotal_events_upper/QED_factor);
+    //hEpEm_QEDnorm->SetBinError(i,sqrt(intotal_events_upper/QED_factor));
+    
+  }
+  
+  
+  
+  //
+  
+  double pole_par[3] = {10, 0.59,0.0144}; //{A,Lambda,Gamma}
+  
+  TF1 *FF_fittepole = new TF1("FF_fittepole", Pole_FFII,0.02,0.92,3);
+  FF_fittepole->SetParameters(&pole_par[0]);
+  FF_fittepole->SetParLimits(0,0.,100.);
+  FF_fittepole->SetParLimits(1,pole_par[1] - pole_par[1]*0.1,pole_par[1] + pole_par[1]*0.1);
+  FF_fittepole->SetParLimits(2,pole_par[2] - pole_par[2]*0.5,pole_par[2] + pole_par[2]*0.5);
+  
+  //for flat
+  double pole_parII[3] = {10, 0.59,0.0144}; //{A,Lambda,Gamma}
+  
+  TF1 *FF_fittepoleII = new TF1("FF_fittepoleII", Pole_FFII,0.02,0.85,3);
+  FF_fittepoleII->SetParameters(&pole_parII[0]);
+  FF_fittepoleII->SetParLimits(0,0.,100.);
+  FF_fittepoleII->SetParLimits(1,pole_parII[1] - pole_parII[1]*0.1,pole_parII[1] + pole_parII[1]*0.1);
+  FF_fittepoleII->SetParLimits(2,pole_parII[2] - pole_parII[2]*0.5,pole_parII[2] + pole_parII[2]*0.5);
+  FF_fittepoleII->SetLineColor(kBlue);
+  //TF1 *FF_fitterGaus = new TF1("FF_fitterGaus","gaus",0.55,0.92);
+  
+  TCanvas *cmkI =  new TCanvas("cmkI","cmkI",1200,500);
+  
+  TH1D *hEpEm_QEDnorm = (TH1D*)in->Get("hEpEm_QEDnorm");
+  cmkI->cd();
+  hEpEm_QEDnorm->SetTitle("Expected distribution of |F(q^{2})|^{2}");
+  hEpEm_QEDnorm->SetLineColor(kBlack);
+  hEpEm_QEDnorm->SetYTitle("|F(q^{2})|^{2}");
+  hEpEm_QEDnorm->GetYaxis()->SetTitleSize(0.05);
+  hEpEm_QEDnorm->GetYaxis()->SetTitleOffset(0.8);
+  hEpEm_QEDnorm->GetXaxis()->SetTitle("M(e^{+}e^{-}) [GeV]");
+  hEpEm_QEDnorm->GetXaxis()->SetTitleSize(0.05);
+  hEpEm_QEDnorm->GetXaxis()->SetTitleOffset(0.8);
+  hEpEm_QEDnorm->Draw("EP");
+  hEpEm_QEDnorm_flat->SetLineColor(8);
+  hEpEm_QEDnorm_flat->Draw("EP same");
+  hEpEm_QEDnorm_flat->Fit("FF_fittepoleII","REM");
+  hEpEm_QEDnorm->Fit("FF_fittepole","REM+");
+
+<<<<<<< HEAD
+  Double_t Lambda = FF_fittepole->GetParameter(1);
+  Double_t Lambdaerr = FF_fittepole->GetParError(1);
+  Double_t bn = 1./Lambda;
+  Double_t bn_err = Lambdaerr/(bn*bn);
+  
+  
+  Double_t Lambda_flat = FF_fittepoleII->GetParameter(1);
+  Double_t Lambdaerr_flat = FF_fittepoleII->GetParError(1);
+  Double_t bn_flat = 1./Lambda_flat;
+  Double_t bn_err_flat = Lambdaerr_flat/(bn_flat*bn_flat);
+  
+  
+  TString sLambda = Form("#Lambda^{2}_{fit}  = %2.4f #pm %2.4f  ", Lambda,Lambdaerr);
+  TString sbn = Form("b_{n} = %2.4f #pm %2.4f  ", bn,bn_err);
+  
+  TString sLambda_gen = Form("#Lambda^{2}_{gen}  = %2.4f  ", 0.5776);
+  TString sbn_gen = Form("b_{n gen} = %2.4f  ", 1./0.5776);
+  
+  TString sLambda_flat = Form("#Lambda^{2}_{fit}  = %2.4f #pm %2.4f  ", Lambda_flat,Lambdaerr_flat);
+  TString sbn_flat = Form("b_{n} = %2.4f #pm %2.4f  ", bn_flat,bn_err_flat);
+>>>>>>> master
 
 
+=======
+>>>>>>> master
   
   TLegend *legmkI = new TLegend(0.12,0.55,0.37,0.9);
   legmkI->SetTextSize(0.04);
@@ -478,16 +771,6 @@ void plot_stuff(){
 
   legmkI_I->Draw("same");
   
-<<<<<<< HEAD
-<<<<<<< HEAD
-  cmkI->Print("figures/counts/result.pdf");
-  cmkIII->Print("figures/counts/acceptance.pdf");
-  cmkII->Print("figures/counts/counts.pdf");
-
-=======
->>>>>>> master
-=======
->>>>>>> master
 }
 
 

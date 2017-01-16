657507d9f9c4e28c47785fbd3ba41b1a48e8bb55
mckunkel <mkunkel@jlab.org>
2017-01-16 12:55:51 +0100

    Added initial draft

--- figures/counts/VMD/edit.C
+++
@@ -1,7 +0,0 @@
-for i in [filename]; do sips -s format [image type] $i --out [destination]/$i.[extension];done
-
-for i in *.png; do sips -s format pdf $i --out $i.pdf;done
-
-for i in *; do mv "$i" "$i.pdf"; done
-
-for f in *.png; do mv "$f" "${f%.*}"; done
